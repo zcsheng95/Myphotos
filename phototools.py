4711e3f5a3048fe503e1f0f2eecdd0e971098c8e
zhechengsheng <zcsheng@ucdavis.edu>
2020-04-16 15:20:19 -0400

    add photos

--- phototools.py
+++ phototools.py
@@ -138,7 +138,7 @@ def handle_photo():
     tmp = bubbleYear(list_info)
     bubble(tmp)
     final_dict = {"list": list_info}
-    with open("../themes/next/source/lib/myPhotos/data.json", "w") as fp:
+    with open("../themes/next/source/lib/album/data.json", "w") as fp:
         json.dump(final_dict, fp)
 
 
